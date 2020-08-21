e1d6c8825bc79d7274eb07da94ad8554b7e88587
tmannazy <e.anene@hotmail.com>
2020-08-21 10:53:32 +0100

    Conversion of Celsius value to Fahrenheit.

--- js-assignments/resources/scripts/main.js
+++ js-assignments/resources/scripts/main.js
@@ -4,7 +4,7 @@ let call = alert('Calculation of Celsius to Fahrenheit');
 // Prompt for Celsius Value
 let value = prompt('Input the Celsius Value');
 
-/**  Fahrenheit calculation **/
+/**  Fahrenheit calculation from Celsius **/
 
 // Fahrenheit Formulae: ((9/5) * C) + 32
 let fahrenheit = ((9 / 5) * value) + 32;
